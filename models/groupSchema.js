185d139e2f35550b7aa013b17baf9ccaf31cdc8d
Jamie Coleman <j.a.coleman1978@gmail.com>
2022-06-23 17:32:51 -0500

    groupSchema built

--- models/groupSchema.js
+++ models/groupSchema.js
@@ -2,7 +2,13 @@ import pkg from 'mongoose';
 const { Schema, model } = pkg;
 
 const GroupSchema = new Schema({
-
+    name: {type: String, required: true},
+    ownerId: {type: Schema.Types.ObjectId, ref: 'User', required: true},
+    memberData: [{
+        id: {type: Schema.Types.ObjectId, ref: 'User', required: true},
+        canEdit: {type: Boolean, required: true}
+    }],
+    openInvitations: [{type: Schema.Types.ObjectId, ref: 'User'}]
 }, {collection: 'groups'});
 
 export default model('Group', GroupSchema);
