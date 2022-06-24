d24d346170da7cf91428af31532b1fe3fc52dd7e
Jamie Coleman <j.a.coleman1978@gmail.com>
2022-06-23 20:25:41 -0500

    settingsSchema built

--- models/settingsSchema.js
+++ models/settingsSchema.js
@@ -2,7 +2,34 @@ import pkg from 'mongoose';
 const { Schema, model } = pkg;
 
 const SettingsSchema = new Schema({
-
+    isDefault: {type: Boolean, required: true},
+    views: {
+        view: {type: String, required: true},
+        subView: {type: String, required: true},
+        startOfWeek: {type: String, required: true},
+        expandedDaysByList: {type: String, required: true},
+        defaultUser: {type: Schema.Types.ObjectId, ref: 'User', required: true}
+    },
+    task: {
+        isIt: {type: Boolean, required: true},
+        priority: {type: String, required: true}
+    },
+    allDay: {
+        isIt: {type: Boolean, required: true},
+        startDate: {type: Date, required: true},
+        endDate: {type: Date, required: true},
+        startTime: {type: Date, required: true},
+        endTime: {type: Date, required: true}
+    },
+    recurring: {
+        isIt: {type: Boolean, required: true},
+        everyNum: {type: Number, required: true},
+        everyUnit: {type: String, required: true},
+        startDate: {type: Date, required: true},
+        endDate: {type: Date, required: true}
+    },
+    peopleAssigned: [{type: Schema.Types.ObjectId, ref: 'User', required: true}],
+    groupsAssigned: [{type: Schema.Types.ObjectId, ref: 'Group', required: true}]
 }, {collection: 'settings'});
 
 export default model('Settings', SettingsSchema);
