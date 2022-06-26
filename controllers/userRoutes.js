2fce59e9b1ffb44bdb66213368959f4fcd172a42
Jamie Coleman <j.a.coleman1978@gmail.com>
2022-06-26 18:04:18 -0500

    Added userRouter

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -1,2 +1,23 @@
 // Dependencies
 import { Router } from "express";
+import UserController from "./userControllers.js";
+
+// Express Router
+const userRouter = Router();
+
+// Route to retrieve user info
+userRouter.route('/login').post(UserController.Login);
+
+// Route to create new user
+userRouter.route('/signup').post(UserController.Signup);
+
+// Route to verify unique username
+userRouter.route('signup/verify').post(UserController.VerifyUniqueUserName);
+
+// Route for verifying session
+userRouter.route('/session').get(UserController.CheckSession);
+
+// Route for loggin out
+userRouter.route('/logout').get(UserController.Logout);
+
+export default userRouter;
