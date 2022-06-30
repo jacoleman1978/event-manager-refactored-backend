4b555132c9643c10474ed23f26cba1f8a0c264b7
Jamie Coleman <j.a.coleman1978@gmail.com>
2022-06-30 14:38:24 -0500

    Made EventController skeleton

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,1 +1,31 @@
 import Event from '../models/eventSchema.js';
+
+export default class EventController {
+    static async addEvent(req, res) {
+
+    }
+
+    static async updateEventInfo(req, res) {
+
+    }
+
+    static async addAssignedUser(req, res) {
+
+    }
+
+    static async removeAssignedUser(req, res) {
+
+    }
+
+    static async addAssignedGroup(req, res) {
+
+    }
+
+    static async removeAssignedGroup(req, res) {
+
+    }
+
+    static async deleteEvent(req, res) {
+        
+    }
+}
